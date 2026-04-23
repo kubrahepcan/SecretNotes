fc4b5fe855c0c5ee1564d7474cc9d506bb84128f
Kübra Hepcan <kubrahepcann@gmail.com>
2026-04-23 15:02:18 +0300

    Improve UI with image and add save functionality for title and secret

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
 from tkinter import *
+from PIL import ImageTk, Image
+
 window = Tk()
+#image
+image = Image.open("secret2.png").convert("RGBA").resize((100, 100))
+img = ImageTk.PhotoImage(image)
+
+image = Label(window, image=img, bg="light pink")
+image.pack(pady=20)
+
 window.title("Secret Notes")
 window.config(bg="light pink")
 window.minsize(width=400, height=700)
 
 #First label
-title = Label(text="Enter your title", bg="light pink", pady=20, font=15)
+title = Label(text="Enter your title", bg="light pink", font=15)
 title.pack()
 #First Entry
-entry1 = Entry()
-entry1.pack(ipadx=80)
+entry1 = Text(width=40, height=1)
+entry1.pack(pady=(0, 20))
 #Second Label
-secret = Label(text="Enter your secret", bg="light pink", pady=20, font=15)
+secret = Label(text="Enter your secret", bg="light pink", font=15)
 secret.pack()
 #Second Entry
-entry2 = Entry()
-entry2.pack(ipadx=80, ipady=80)
+entry2 = Text(width=40, height=10)
+entry2.pack(pady=(0, 20))
 #Third label
-masterkey = Label(text="Enter your key", bg="light pink", pady=20, font=15)
+masterkey = Label(text="Enter your key", bg="light pink", font=15)
 masterkey.pack()
 #Third Entry
-entry3 = Entry()
-entry3.pack(ipadx=80)
+entry3 = Text(width=40, height=1)
+entry3.pack(pady=(0, 20))
+
+
+##########################################
+def save_title():
+    title = entry1.get()
+
+    with open("Notes.txt", "a") as file:
+        file.write(title + "\n")
+
+
+def save_secret():
+    secret = entry2.get()
+
+    with open("Notes.txt", "a") as file:
+        file.write(secret + "\n")
+
+
+def all_entries():
+    save_title()
+    save_secret()
+
+
 #Save Button
-button = Button(text="Save & Encrypt")
+button = Button(text="Save & Encrypt", command=all_entries)
 button.pack(pady=20)
 #Decrypt Button
 button2 = Button(text="Decrypt")
